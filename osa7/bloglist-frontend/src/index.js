c75b816a1c2df470a218e5f6ed63c8fb8867bc27
lauripalonen <palonen.lauri@gmail.com>
2020-06-04 21:26:52 +0300

    capsulate redux store

--- osa7/bloglist-frontend/src/index.js
+++ osa7/bloglist-frontend/src/index.js
@@ -2,11 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import { createStore } from 'redux'
+import store from './store'
 import { Provider } from 'react-redux'
-import notificationReducer from './reducers/notificationReducer'
-
-const store = createStore(notificationReducer)
 
 ReactDOM.render(
   <Provider store={store}>
