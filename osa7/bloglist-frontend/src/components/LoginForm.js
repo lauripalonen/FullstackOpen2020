e1e7d5683badd5cd4dad5942e80b9f40207c6603
lauripalonen <palonen.lauri@gmail.com>
2020-07-01 20:17:16 +0300

    apply materialUi on login page

--- osa7/bloglist-frontend/src/components/LoginForm.js
+++ osa7/bloglist-frontend/src/components/LoginForm.js
@@ -1,39 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useDispatch } from 'react-redux'
 import { userLogin } from '../reducers/loginReducer'
 import { useHistory } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+
+import { TextField, Button } from '@material-ui/core'
 
 const LoginForm = () => {
+  const [values, setValues] = useState({ username: '', password: '' })
+
   const dispatch = useDispatch()
   const history = useHistory()
 
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
-    const username = event.target.username.value
-    const password = event.target.password.value
-
-    dispatch(userLogin(username, password))
-
-    event.target.username.value = ''
-    event.target.password.value = ''
+    dispatch(userLogin(values.username, values.password))
+    setValues({ username: '', password: ''})
     history.push('/blogs')
   }
 
   return (
     <div>
       <h2>log in to application</h2>
-      <Form onSubmit={handleLogin}>
-        <Form.Group>
-          <Form.Label>username:</Form.Label>
-          <Form.Control type="text" name="username"/>
-          <Form.Label>password:</Form.Label>
-          <Form.Control type="password" name="password"/>
-          <Button variant="primary" type="submit">login</Button>
-        </Form.Group>
-      </Form>
-    </div>
+      <form onSubmit={handleLogin} >
+        <div>
+          <TextField label="username" onChange={handleChange('username')} />
+        </div>
+        <div>
+          <TextField label="password" type='password' onChange={handleChange('password')} />
+        </div>
+        <div>
+          <Button variant="contained" color="primary" type="submit" >
+            login
+          </Button>
+        </div>
+      </form>
+      {/* <Form onSubmit={handleLogin}>
+      <Form.Group>
+        <Form.Label>username:</Form.Label>
+        <Form.Control type="text" name="username" />
+        <Form.Label>password:</Form.Label>
+        <Form.Control type="password" name="password" />
+        <Button variant="primary" type="submit">login</Button>
+      </Form.Group>
+    </Form> */}
+    </div >
   )
 }
 
