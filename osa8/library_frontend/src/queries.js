5004139a1033a9d0b5571124ca7ddd1f5770701c
lauripalonen <palonen.lauri@gmail.com>
2020-07-10 11:55:01 +0300

    do task 8.10

--- osa8/library_frontend/src/queries.js
+++ osa8/library_frontend/src/queries.js
@@ -10,12 +10,28 @@ export const ALL_AUTHORS = gql`
   }
 `
 
-export const ALL_BOOKS= gql`
+export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
       published
       author
+      genres
+    }
+  }
+`
+
+export const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+      published
       genres
     }
   }
