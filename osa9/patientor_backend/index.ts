1b1c60f23c4ef6be0b2c103a8e2e2cdb69d704cf
lauripalonen <palonen.lauri@gmail.com>
2020-08-13 13:08:15 +0300

    do task 9.9

--- osa9/patientor_backend/index.ts
+++ osa9/patientor_backend/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-const app = express();
-app.use(express.json());
+import cors from 'cors';
 
-app.get('/ping', (_req, res) => {
+const app = express();
+
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+app.get('/api/ping', (_req, res) => {
   console.log('received ping request');
   res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
